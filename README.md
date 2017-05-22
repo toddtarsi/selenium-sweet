c2735b3186f4db158d094072f767b07b4abb0180
toddtarsi <Eplata@Todds-MacBook-Pro.local>
2017-05-22 09:52:37 -0500

    Better organization of files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# selenium-example-tests
+# Selenium Suite:
+
+The purpose of this repo is twofold.
+
+1. Help other Selenium developers, by making test maintenance a bit easier.
+2. Showcase the immense amount of technology inside of Selenium builder / IDE, as this is largely the gutted transpiler internals of SE.
+
+# How to use this repo:
+
+Clone or fork this repo, and start storing your tests in lib/tests.
+By maintaining all of these tests as JSON, we're able to separate any test issues from implementation issues, and can truly own the process of composing our output script.
+
+You can set the output format by filename in lib/config/index.js, and transpile with npm build;
+
+If you'd like to use the transpiler with a one off format, you can run the transpiler directly from the command line with `node scripts/transpile -f [format]`. If format isn't provided, your format from config will be used.
--- scripts/transpile.js
+++ scripts/transpile.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const { createLangFormatter, parseScript } = require('../lib/internals/transpiler');
+const defaultFormat = require(
+  path.join(__dirname, '..', 'lib', 'config')
+).format;
+const { createLangFormtter, parseScript } = require(
+  path.join(__dirname, '..', 'lib', 'server', 'transpiler')
+);
 
 const program = require('commander');
-
-const testRoot = path.join('lib', 'tests');
 const formatsRoot = path.join('lib', 'formats');
-const availableTests = fs.readdirSync(testRoot);
 const availableFormats = fs.readdirSync(formatsRoot);
+
 const formatOptions = availableFormats
   .map(r => r.slice(0, -3))
   .join('|');
@@ -17,16 +20,23 @@ program
   .option(
     '-f --format <format>',
     `Output format - options: ${formatOptions}`,
-    new RegExp(`^(${formatOptions})$`, 'i')
+    new RegExp(`^(${formatOptions})$`, 'i'),
+    defaultFormat
   )
   .parse(process.argv);
+
 if(!program.format) {
-  console.info("No output format selected, please select an output format");
-  console.info("Available formats:", formatOptions.replace(/\|/g, ', '));
+  console.warn("No output format selected.");
+  console.warn("Please specify an output format with -f [format]");
+  console.warn("Available formats:");
+  console.warn("  " + formatOptions.replace(/\|/g, '\n  '));
   return process.exit(1);
 }
+
 const formatter = require(path.join('..', formatsRoot, program.format + '.js'));
 
+const testRoot = path.join('lib', 'tests');
+const availableTests = fs.readdirSync(testRoot);
 availableTests.forEach(testName => {
   if (testName.slice(-5) !== '.json') return;
   const script = parseScript(fs.readFileSync(path.join(testRoot, testName)));
