6989e54a56edf091ff0d15e47b355f592ea08bb8
toddtarsi <Eplata@Todds-MacBook-Pro.local>
2017-07-12 16:41:04 -0500

    Simplifying transpile help messaging

--- packages/se-sweet-example-repo/scripts/transpile.js
+++ packages/se-sweet-example-repo/scripts/transpile.js
@@ -5,16 +5,16 @@ const { transpileDir } = require('se-builder-transpiler');
 const defaultFormat = require(path.join(__dirname, '..', 'config')).format;
 const formatsRoot = path.join(__dirname, '..', 'formats');
 const availableFormats = fs.readdirSync(formatsRoot);
-
-const formatOptions = availableFormats
+const spaces = '            ';
+const formatOptions = '\n' + spaces + availableFormats
   .map(r => r.slice(0, -3))
-  .join('|');
+  .join('\n' + spaces);
 
 program
   .version('0.0.1')
   .option(
     '-f --format <format>',
-    `Output format - options: ${formatOptions}`,
+    `Output format. By default, will derive from format key in config/index. Available options: ${formatOptions}`,
     new RegExp(`^(${formatOptions})$`, 'i'),
     defaultFormat
   )
@@ -28,6 +28,7 @@ if(!program.format) {
   return process.exit(1);
 }
 
+console.log("Transpiling to " + program.format);
 return transpileDir(
   path.join(__dirname, '..', 'formats', program.format + '.js'),
   path.join(__dirname, '..', 'tests', 'lib'),
